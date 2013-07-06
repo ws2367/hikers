80b23871dd9be310e49f548706224d3a2ee845e7
Shawn Shaw <de54714@gmail.com>
2013-07-06 14:37:05 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hikers::Application.config.secret_key_base = 'e654b6d2cb8d9c9bfc05fd5a36496201e27313ba59e8fdecb6caebe848de2b4f7d753426a91da17437baef3230f8d67b4da5ba75144cf1d0aee817c9aeea0422'
