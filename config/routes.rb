929c48b088766d9ab0710d82170d2ba030b648de
Shawn Shaw <de54714@gmail.com>
2013-07-15 00:17:59 -0700

    Reroute root to pins#index

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Hikers::Application.routes.draw do
 
   get 'about' => 'pages#about'
 
-  root :to => "pages#home"
+  root :to => "pins#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
