ea19686b552e3982da51eb31220afc57cea1d469
Shawn Shaw <de54714@gmail.com>
2013-07-16 20:22:47 -0700

    Add Image Uploading via URL

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,5 @@
 class Pin < ActiveRecord::Base
-  	attr_accessible :description, :image
+  	attr_accessible :description, :image, :image_remote_url
 
   	belongs_to :user
   	validates :user_id, presence: true
@@ -9,4 +9,10 @@ class Pin < ActiveRecord::Base
 								size: { less_than: 5.megabytes} 
   
   	has_attached_file :image, styles: {medium: "320x240>"}
+
+  	def image_remote_url=(url_value) 
+  		self.image = URI.parse(url_value) unless url_value.blank?
+  		super 
+  	end
+
 end
