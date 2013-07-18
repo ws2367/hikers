629708e4ad0cfa80f55b09b09e671df9d0605a33
Shawn Shaw <de54714@gmail.com>
2013-07-18 00:52:13 -0700

    Add user show endless scroll

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -21,6 +21,7 @@ class PinsController < ApplicationController
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @pin }
+      format.js
     end
   end
 
