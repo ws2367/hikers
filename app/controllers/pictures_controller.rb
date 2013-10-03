03f74c6edf94301736da1270f457f34b758fdfba
Shawn Shaw <wenhsiang.shaw@gmail.com>
2013-10-03 12:13:24 -0400

    Change to image uploading

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -6,22 +6,23 @@ class PicturesController < ApplicationController
   # POST /pictures/:post_id
   def create
   	# using create(..), if it cannot be saved in db, id is nil
+     
   	@pic = Picture.create(post_id: params[:post_id].to_i, 
   		                  img: request.body, img_content_type: request.content_type)
   	
   	# Picture model validates whether the assoicated post exists
 	respond_to do |format|
-		format.json { render json: { "status" => @pic.id? ? "success" : "fail"} }
+		format.json { render json: { "status" => @pic.id? ? "success" : "fail", "index" => "#{@pic.post.pictures.index(@pic)}"} }
 	end
   end
 
   #GET /pictures/:post_id/:index
   def show
   	@post = Post.find(params[:post_id].to_i)
-
+    logger.info("Post id=#{@post.id} is found.")
   	@pic  = @post.pictures[params[:index].to_i]
-  	
-  	send_file @pic.img.url, :type => @pic.img_content_type
+  	logger.info("Picture id=#{@pic.id} is found.")
+  	redirect_to @pic.img.url
   end
 
   def destroy
