161552143f1b7ce20f769919fa9238587a479bbc
Iru <iru@adolphlindembp.gateway.pace.com>
2014-03-24 09:20:53 -0700

    Fix to_sym undefined

--- db/migrate/20140323210304_add_fb_related_columns_to_users.rb
+++ db/migrate/20140323210304_add_fb_related_columns_to_users.rb
@@ -1,6 +1,6 @@
 class AddFbRelatedColumnsToUsers < ActiveRecord::Migration
   def change
-    change_column :users, :user_name,          :null => true, :default => ""
-    change_column :users, :encrypted_password, :null => true, :default => ""
+    change_column :users, :user_name,  :string,        :null => true, :default => ""
+    change_column :users, :encrypted_password, :string, :null => true, :default => ""
   end
 end
