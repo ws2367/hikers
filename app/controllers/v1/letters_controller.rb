ae7b091b73979f1ac52561d0375c1030e63cf1a4
Shawn <s@yoursapp.cc>
2014-05-12 15:13:38 -0700

    Fix the bug in letter request

--- app/controllers/v1/letters_controller.rb
+++ app/controllers/v1/letters_controller.rb
@@ -3,6 +3,6 @@ class V1::LettersController < ApplicationController
   # GET /letters/check
   def check
     render :status => 200,
-           :json => {:message => "Welcome! %s" % request.user_agent }
+           :json => {:letter => "Welcome! %s" % request.user_agent }
   end  
 end
