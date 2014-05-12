25b7770f8975966e2936b04181191541cebf5aeb
Shawn <s@yoursapp.cc>
2014-05-12 15:29:29 -0700

    Extract version number in letter controller

--- app/controllers/v1/letters_controller.rb
+++ app/controllers/v1/letters_controller.rb
@@ -2,7 +2,13 @@ class V1::LettersController < ApplicationController
   
   # GET /letters/check
   def check
+    version_number = request.user_agent[/Yours\/\d+/][6..-1].to_i
+    logger.info("Client App Version: %i" % version_number)
+    # if version_number == 313
+    #   render :status => 200,
+    #          :json => {:letter => "Welcome! %s" % request.user_agent }
+    # end
     render :status => 200,
-           :json => {:letter => "Welcome! %s" % request.user_agent }
+           :json => {}
   end  
 end
